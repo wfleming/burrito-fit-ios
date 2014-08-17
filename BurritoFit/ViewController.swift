3f64cb454ef865e7f9b429e9180b64e55fec7d0a
Will Fleming <will@mintdigital.com>
2014-08-17 11:39:16 -0400

    Initial Commit

---
+++ BurritoFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BurritoFit
+//
+//  Created by Will Fleming on 8/17/14.
+//  Copyright (c) 2014 Will Fleming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
