f12a7fd9d2653ee1632be0e1615cfb3ff89ed106
Will Fleming <will@mintdigital.com>
2014-08-17 13:20:06 -0400

    initial setup

---
+++ BurritoFit/BurritoFit-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  BurritoFit-Bridging-Header.h
+//  BurritoFit
+//
+//  Created by Will Fleming on 8/17/14.
+//  Copyright (c) 2014 Will Fleming. All rights reserved.
+//
+
+#import <ZeroPush/ZeroPush.h>
+#import <AFNetworking/AFNetworking.h>
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '7.0'
+
+link_with 'BurritoFit', 'BurritoFitTests'
+
+pod 'AFNetworking'
+pod 'GoogleAnalytics-iOS-SDK'
+pod 'ZeroPush'
